1829313b6edc1588ad76e95c13e90e6081fe6709
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-11-27 17:51:19 +0900

    * add /weather-data API. * update gitignore.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const app = express();
-const PROXY_PORT = 5000;
+const PROXY_PORT = 5080;
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const rateLimit = require('express-rate-limit');
 require('dotenv').config();
+const url = require('url');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -16,6 +17,17 @@ app.use(limiter);
 
 app.get('/', (req, res) => {
   res.send('This is my proxy server');
+});
+
+app.use('/weather-data', (req, res, next) => {
+  const city = url.parse(req.url).query;
+  createProxyMiddleware({
+    target: `${process.env.BASE_API_URL_WEATHERAPI}${city}&api=no`,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/weather-data`]: '',
+    },
+  })(req, res, next);
 });
 
 app.use('/corona-tracker-world-data', (req, res, next) => {
