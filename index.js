970d4829e29e3902d1fd40960491593d41ea9b63
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-11-27 18:29:43 +0900

    * fix removed variable name.

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use('/corona-tracker-world-data', (req, res, next) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(PROXY_PORT, () => {
+app.listen(port, () => {
   console.log(`Listening on localhost port ${port}`);
 });
 
